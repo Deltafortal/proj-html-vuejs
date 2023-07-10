5803fc875d7d3a6db241a4438168ca38fe67e688
Deltafortal <aldolucchetta21@gmail.com>
2023-07-10 16:22:42 +0200

    Imported font and fontawesome

--- src/main.js
+++ src/main.js
@@ -1,4 +1,17 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import '@fontsource/roboto/900.css';
+
+/* import fontawesome component and core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+
+/* Import Icons */
+import { faCopyright } from '@fortawesome/free-solid-svg-icons'
+
 
 createApp(App).mount('#app')
