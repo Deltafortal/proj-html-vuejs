12dfe222cc282097086e4f5c29e379a68233f05a
Deltafortal <aldolucchetta21@gmail.com>
2023-07-10 12:15:15 +0200

    Header Layout Sketch

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,69 @@
+<!--------------- Script ----------------->
+
+<script setup>
+
+</script>
+
+
+
+
+
+
+
+
+<!----------------- Template ------------------>
+
+<template>
+  
+    <!----- Icon ----->
+    <div class="icon">
+        <img src="" alt="logo">
+    </div>
+
+
+
+    <!---- NavBar ----->
+    <nav>
+        <ul>
+            <li>Home</li>
+        </ul>
+    </nav>
+
+    
+    <!---- Header Menu ---->
+    <div class="header-menu">
+
+
+        <!-- Hamburgher Menu-->
+        <HamburgherMenu />
+
+        <!-- Events Button-->
+        <div class="event-btn button">
+
+            <img src="" alt="Helmet">
+            <div>Upcoming Events</div>
+            <img src="../assets/img/free-fa-solid-900.svg-fonta.svg" alt="Arrow">
+
+        </div>
+
+
+    </div>
+
+</template>
+
+
+
+
+
+
+
+
+<!------------------ Style ----------------->
+
+<style lang="scss" scoped>
+
+// Import
+@use './assets/scss/style.scss'
+
+
+</style>
