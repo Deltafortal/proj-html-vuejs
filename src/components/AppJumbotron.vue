e77cf5c81e2df42aa697fa8770bdf9d81a8585cc
Deltafortal <aldolucchetta21@gmail.com>
2023-07-10 12:23:29 +0200

    Jumbotron layout sketch

---
+++ src/components/AppJumbotron.vue
@@ -0,0 +1,66 @@
+
+<!---------- Script ------------>
+
+<script setup>
+
+</script>
+
+
+
+
+
+
+
+
+<!------------ Template ------------->
+
+<template>
+  
+    <!---- Jumbotron ------>
+    <div class="jumbotron">
+
+
+        <!-- Left Arrow -->
+        <div class="button">
+
+        </div>
+
+        <!-- Content -->
+        <div class="content">
+            <h2>Dinamic Title</h2>
+            <h4>Learn cycling from the pros</h4>
+
+            <div class="button">
+                Learn more
+                <img src="" alt="Right Arrow">
+            </div>
+
+        </div>
+
+        <!-- Right Arrow-->
+        <div class="button">
+            
+        </div>
+
+
+
+    </div>
+    
+</template>
+
+
+
+
+
+
+
+
+<!-------------- Style ------------>
+
+<style lang="scss" scoped>
+
+// Import
+@use './assets/scss/style.scss'
+
+
+</style>
